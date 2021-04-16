5775de0852926c1e7d6ee5dd2180822eaf56ac4e
Dillon Coffey <dcoffey@nd.edu>
2021-04-16 13:03:09 -0400

    Quad tree and node files

---
+++ inc/quadTree.h
@@ -0,0 +1,18 @@
+#ifndef QUADTREE_H
+#define QUADTREE_H
+
+#include <cstdlib>
+#include <iostream>
+
+#include "quadTreeNode.h"
+#include "satellite.h"
+
+
+template<class T>
+class quadTree{
+
+	private:
+
+			
+};
+#endif
---
+++ inc/quadTreeNode.h
@@ -0,0 +1,15 @@
+#ifndef QUADTREENODE_H
+#define QUADTREENODE_H
+
+#include <cstdlib>
+#include <iostream>
+
+#include "satellite.h"
+
+template<class T>
+class quadTreeNode{
+
+
+};
+
+#endif
