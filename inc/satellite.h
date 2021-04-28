ebe413ff279f960d71266c6f0fd2b962cbe80f0b
= <ecrowle2@nd.edu>
2021-04-27 20:22:33 -0400

    first satellite class push

---
+++ inc/satellite.h
@@ -0,0 +1,21 @@
+#ifndef NODE_H
+#define NODE_H
+
+#include <cstdlib> // for NULL
+
+class Satellite{
+	
+	private:
+		Point position;
+				
+
+		
+	public:
+	// constructors
+	// destructors
+	// functions to determine edges of field of view
+
+
+};
+
+#endif
